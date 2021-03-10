b7d52069a7faebfe1bef298784d6b1e1d24dd350
Francisco Sebastião <f.2018pedro@gmail.com>
2021-03-10 11:52:50 +0100

    pequeno app em python

---
+++ app.py
@@ -0,0 +1,10 @@
+x_company = 'FREECompany'
+lista = []
+#Interação para pegar cada letra da variavel name
+
+for l in x_company: 
+# Add cada letra da variavel name a variavel lista por meio do loop for com a variavel l
+    lista.append(l)
+
+# Mostrando o resultado
+print(lista)
